a26b1734998e50b759365e73c40845bb31de7d8f
Michael Brown <brownieboy@gmail.com>
2015-08-27 15:38:41 +1000

    Added move icon via CSS when sortable.

--- app/components/ListComponent.jsx
+++ app/components/ListComponent.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class ListComponent extends React.Component {
 	render() {
-      return (<li key={this.props.key}>item {this.props.item} innit</li>);
+      return (<li key={this.props.key}>item {this.props.item}</li>);
 	}
 }
