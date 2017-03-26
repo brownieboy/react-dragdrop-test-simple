e510c06a700c2094cc86495486add036a244876d
Michael Brown <brownieboy@gmail.com>
2017-03-26 20:45:10 +1100

    ComponentsList tidy up

--- app/components/ListOfComponents.jsx
+++ app/components/ListOfComponents.jsx
@@ -1,6 +1,7 @@
 /* eslint-env jquery */
 import React from "react";
-import ReactDom from "react-dom";
+
+// import ReactDom from "react-dom";
 // import $ from 'jquery';
 // import 'jquery-ui/sortable';
 import "jquery-ui/ui/widgets/sortable.js";
@@ -9,6 +10,32 @@ import "jquery-ui/ui/widgets/sortable.js";
 import ListComponent from "./ListComponent.jsx";
 
 export default class ComponentsList extends React.Component {
+  componentDidMount() {
+    this.$node = $(this.refs.sortable);
+    this.makeSortable(this.$node);
+  }
+
+  makeSortable() {
+    if (this.props.sortable) {
+      const that = this;
+      const connectionString = `#${this.props.id}`;
+
+      $(connectionString).sortable({
+        helper: "clone",
+        start(event, ui) {
+          that.props.onItemDragStart(this, event, ui);
+        },
+        stop(event, ui) {
+          that.props.onItemDragStop(this, event, ui);
+        }
+      });
+    }
+  }
+
+  getId() {
+    return this.props.id;
+  }
+
   render() {
     if (this.props.items.length === 0) {
       return (
@@ -19,7 +46,9 @@ export default class ComponentsList extends React.Component {
     return (
       <section className={this.props.sectionClassName}>
         <h2>{this.props.title}</h2>
-        <ul id={this.props.id} className="connected-list index-list">
+        <ul id={this.props.id}
+          ref="sortable"
+          className="connected-list index-list">
           {this.props.items.map(item =>
               <ListComponent key={item.key} title={item.title} />
           )}
@@ -27,35 +56,17 @@ export default class ComponentsList extends React.Component {
       </section>
     );
   }
-
-  componentDidMount() {
-    const thisDOMNode = ReactDom.findDOMNode(this);
-
-    this.makeSortable(thisDOMNode);
-  }
-
-  makeSortable() {
-    if (this.props.sortable) {
-      const that = this;
-      const connectionString = `#${this.props.id}`;
-
-      $(connectionString).sortable({
-        helper: "clone",
-        start: function(event, ui) {
-          that.props.onItemDragStart(this, event, ui);
-        },
-        stop: function(event, ui) {
-          that.props.onItemDragStop(this, event, ui);
-        }
-      });
-    }
-  }
-
-  getId() {
-    return this.props.id;
-  }
 }
 
 ComponentsList.defaultProps = {
   items: []
 };
+
+ComponentsList.propTypes = {
+  id: React.PropTypes.string.isRequired,
+  items: React.PropTypes.array.isRequired,
+  sectionClassName: React.PropTypes.string,
+  sortable: React.PropTypes.bool.isRequired,
+  title: React.PropTypes.string
+};
+
